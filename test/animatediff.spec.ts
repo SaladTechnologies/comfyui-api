a02f39be532451bd90d7f416d1df8715a6e2d128
Shawn <shawn.rushefsky@gmail.com>
2025-01-07 10:03:26 -0500

    start outlining tests

---
+++ test/animatediff.spec.ts
@@ -0,0 +1,7 @@
+import animateDiffVideoAndFramesWorkflow from "./workflows/animatediff-video-and-frames.json";
+
+describe("AnimateDiff", () => {
+  it("returns still frames and a video", () => {});
+  it("returns just a video", () => {});
+  it("returns just still frames", () => {});
+});
